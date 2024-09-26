efc18589542852ae0edebcd7529f02fc5d06b991
Marowitski <47811093+Marowitski@users.noreply.github.com>
2024-09-26 15:56:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ document.querySelectorAll('.region').forEach(region => {
         const infoBox = document.getElementById('infoBox');
         const infoContent = document.getElementById('infoContent');
 
-        // Tıklanan mahalleye ait bilgiyi al
+        // Tıklanan bölgeye ait bilgiyi al
         infoContent.textContent = this.getAttribute('data-info');
 
-        // Bilgi kutusunu butonun hemen altına yerleştir
-        const rect = this.getBoundingClientRect(); // Butonun pozisyonunu al
-        const x = rect.left + window.scrollX; // Butonun X pozisyonu
-        const y = rect.bottom + window.scrollY; // Butonun alt kısmının Y pozisyonu
+        // Tıklanan butonun konumunu al
+        const rect = this.getBoundingClientRect();
 
-        infoBox.style.left = x + 'px';
-        infoBox.style.top = y + 'px';
-        infoBox.style.display = 'block';
+        // Bilgi kutusunu butonun hemen altına yerleştir
+        infoBox.style.left = rect.left + 'px';  // Butonun sol kenarı
+        infoBox.style.top = rect.bottom + 'px'; // Butonun alt kısmı
+        infoBox.style.display = 'block';        // Bilgi kutusunu göster
     });
 });
 
