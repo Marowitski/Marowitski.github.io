0042d724404f0688023c877e08fc31faf2fd5c06
Marowitski <47811093+Marowitski@users.noreply.github.com>
2024-09-26 15:51:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 document.querySelectorAll('.region').forEach(region => {
-    region.addEventListener('click', function() {
+    region.addEventListener('click', function(event) {
         const infoBox = document.getElementById('infoBox');
         const infoContent = document.getElementById('infoContent');
         
-        // Tıklanan bölgeye ait bilgiyi al
+        // Tıklanan mahalleye ait bilgiyi al
         infoContent.textContent = this.getAttribute('data-info');
 
-        // Kutucuğu göster
+        // Kutucuğu tıklanan yerde göster
+        const x = event.pageX;
+        const y = event.pageY;
+
+        infoBox.style.left = x + 'px';
+        infoBox.style.top = y + 'px';
         infoBox.style.display = 'block';
+    });
+});
+
+// Bilgi balonundaki butonlara tıklama olayları
+document.querySelectorAll('.info-option').forEach(button => {
+    button.addEventListener('click', function() {
+        alert(this.textContent + ' butonuna tıkladınız.');
     });
 });
